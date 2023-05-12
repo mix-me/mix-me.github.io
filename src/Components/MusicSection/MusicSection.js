d6b2b74e8738f9d11302e7f82b4dd0283a97e513
Reality Audio Studio Dev <87328737+realityaudiostudio@users.noreply.github.com>
2023-05-12 05:31:55 +0000

    majors

--- src/Components/MusicSection/MusicSection.js
+++ src/Components/MusicSection/MusicSection.js
@@ -1,8 +1,23 @@
+import { useState, useEffect } from "react";
 import "./musicsection.css";
+import {db} from "../../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 function MusicSection() {
+    const [songs1,setSongs1] = useState([]);
+    const songsCollectionRef = collection(db, "songs1");
+    useEffect(() => {
+        
+       const getSongs1 = async () => {
+        const data = await getDocs(songsCollectionRef);
+        console.log(data);
+        
+       }
+        
+        getSongs1()
+    }, [])
     return (
-        <div><p className="subh">What's new | <br></br><a href="release.html" style={{fontSize: "10px",textAlign: "left"}}>View more</a></p>
+        <div><p className="subh">What's new |</p>
         <div className="scrollmenu">
 <div className="card"><a href="https://realityaudiostudio.github.io/mixme2/sng9.html"><img src="https://realityaudiostudio.github.io/img/kalimg.png" alt="alanjs" className="deti"></img></a><br></br>Kaalpanthin</div>
         </div></div>
