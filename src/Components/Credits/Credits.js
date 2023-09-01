a68afd39f6e7190b508ead191c4ec453e950d3ef
Alan Jose Santo <87328737+realityaudiostudio@users.noreply.github.com>
2023-09-01 17:33:32 +0000

    workbuild

---
+++ src/Components/Credits/Credits.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./credits.css";
+function Credits() {
+  return (
+    <div className='cred'>
+        <p className='chead'>This is Mix Me</p>
+        <p className='csb'>The place where music find it’s best !</p>
+        <div className='vara'></div>
+        <img className='logs' src='https://drive.google.com/uc?export=download&id=1r5iTAkWDToyh--QC2Pa4qoXUr-kCjRQL'></img>
+        <p className='vishadham'>Mix Me is an innovative music streaming app that has been developed by the talented team at AJS WEB CREATIVES. Designed to provide a seamless and enjoyable music experience, Mix Me stands out from the crowd by offering a vast library of free original music releases from Reality Audio Studio.
+<br></br>
+The app's interface is user-friendly and visually appealing, ensuring that users can navigate effortlessly through its extensive music catalog. Whether you're looking for soulful ballads, energetic rock anthems, or genre-bending experimental tracks, Mix Me has it all.
+<br></br>
+
+In addition to its impressive music library, Mix Me provides users with exclusive content, such as artist interviews, behind-the-scenes footage, and live performance recordings. Furthermore, Mix Me offers a seamless integration with other devices and platforms, ensuring that users can enjoy their favorite music on the go. 
+<br></br>
+With Mix Me, AJS WEB CREATIVES has created a music streaming app that combines technological innovation, artistic expression, and a passion for original music. Whether you're an audiophile or simply someone who loves discovering new music, Mix Me is the perfect companion to embark on a captivating musical journey.</p>
+    </div>
+  )
+}
+
+export default Credits;
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { IonContent } from '@ionic/react';
+import Credits from '../Components/Credits/Credits';
 
 const SearchPage = () => (
   <>
     <IonContent>
       <div>
-        Search content
+        <Credits></Credits>
       </div>
     </IonContent>
   </>
