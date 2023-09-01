626d0ab9af6eb46256cf1f1ca2277d2b6bb2e68c
Alan Jose Santo <87328737+realityaudiostudio@users.noreply.github.com>
2023-09-01 15:04:05 +0000

    timestamp

--- src/Components/MusicSection/MusicSection.js
+++ src/Components/MusicSection/MusicSection.js
@@ -1,33 +1,45 @@
 import { useState, useEffect } from "react";
 import "./musicsection.css";
-import {db} from "../../firebase-config";
+import { db } from "../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 
 function MusicSection() {
-    const [songs1,setSongs1] = useState([]);
-    const songsCollectionRef = collection(db, "new");
-    useEffect(() => {
-        
-       const getSongs1 = async () => {
-        const data = await getDocs(songsCollectionRef);
-        setSongs1(data.docs.map((doc) => ({...doc.data() , id: doc.id })));
-        
-       };
-        
-        getSongs1()
-    }, [])
-    return (
-        <div>
-            <p className="subh">What's new |</p>
-        <div className="scrollmenu">
-            {songs1.map((songs) => {
-                return (<div>
-                    
-<div className="card"><a href={songs.link}><img src={songs.image} alt="alanjs" className="deti"></img></a><br></br><p>{songs.name}</p></div>
-        </div> 
-            )})}
-            </div></div>
-    )   
+  const [songs1, setSongs1] = useState([]);
+  const songsCollectionRef = collection(db, "new");
+
+  useEffect(() => {
+    const getSongs1 = async () => {
+      const data = await getDocs(songsCollectionRef);
+      const sortedData = data.docs
+        .map((doc) => ({ ...doc.data(), id: doc.id }))
+        .sort((a, b) => b.timestamp - a.timestamp); // Change 'timestamp' to the field you want to sort by
+
+      setSongs1(sortedData);
+    };
+
+    getSongs1();
+  }, []);
+
+  return (
+    <div>
+      <p className="subh">What's new |</p>
+      <div className="scrollmenu">
+        {songs1.map((song) => {
+          return (
+            <div>
+              <div className="card">
+                <a href={song.link}>
+                  <img src={song.image} alt="alanjs" className="deti" />
+                </a>
+                <br></br>
+                <p>{song.name}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
-  
-  export default MusicSection;
+
+export default MusicSection;
